c92d0300e2225b9e92209f5a898bc29d4190cdf6
Victor Laitila <victorlaitila@MacBook-Pro-som-tillhor-Victor.local>
2025-02-10 15:02:45 +0200

    Delete and put + tests

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -12,4 +12,21 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(result)
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+  const updatedBlogPost = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true, runValidators: true })
+  response.json(updatedBlogPost)
+})
+
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const Blog = require('../models/blog')
 const blogData = require('./blogData.json')
@@ -8,92 +8,146 @@ const app = require('../app')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  const blogObjects = blogData.blogs.map(blog => new Blog(blog))
-  const promiseArray = blogObjects.map(blog => blog.save())
-  await Promise.all(promiseArray)
-})
-
-test('returns correct number of blog posts in json format', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-  assert.strictEqual(response.body.length, blogData.blogs.length)
-})
-
-test('unique identifier for blog posts is named id', async () => {
-  const response = await api.get('/api/blogs')
-  response.body.forEach(blog => {
-    const keys = Object.keys(blog)
-    assert(keys.includes('id'))
-    assert(!keys.includes('_id'))
+describe('blog api', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    const blogObjects = blogData.blogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
   })
-})
+  
+  test('returns correct number of blog posts in json format', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    assert.strictEqual(response.body.length, blogData.blogs.length)
+  })
+  
+  test('unique identifier for blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
+    response.body.forEach(blog => {
+      const keys = Object.keys(blog)
+      assert(keys.includes('id'))
+      assert(!keys.includes('_id'))
+    })
+  })
+  
+  test('new blog post is created successfully', async () => {
+    const newBlogPost = {
+      title: 'New Blog',
+      author: 'New Author',
+      url: 'newblog.com',
+      likes: 0
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlogPost)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(blog => blog.title)
+    assert(titles.includes('New Blog'))
+    assert.strictEqual(response.body.length, blogData.blogs.length + 1)
+  })
+  
+  test('creating new blog post without likes defaults number of likes to 0', async () => {
+    const newBlogPost = {
+      title: 'Blog Without Likes',
+      author: 'Test Author',
+      url: 'nolikes.com'
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlogPost)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    const response = await api.get('/api/blogs')
+    const addedPost = response.body.find(blog => blog.title === 'Blog Without Likes')
+    assert.strictEqual(addedPost.likes, 0)
+  })
+  
+  test('if title is missing from new blog post, backend responds with status code 400', async () => {
+    const newBlogPost = {
+      author: 'Test Author',
+      url: 'notitle.com',
+      likes: 0
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlogPost)
+      .expect(400)
+  })
+  
+  test('if url is missing from new blog post, backend responds with status code 400', async () => {
+    const newBlogPost = {
+      title: 'Test Title',
+      author: 'Test Author',
+      likes: 0
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlogPost)
+      .expect(400)
+  })
 
-test('new blog post is created successfully', async () => {
-  const newBlogPost = {
-    title: 'New Blog',
-    author: 'New Author',
-    url: 'newblog.com',
-    likes: 0
-  }
+  describe('deleting blog posts', () => {
+    test('successful deletion of existing blog post', async () => {
+      const blog = blogData.blogs[0]
+      await api
+        .delete(`/api/blogs/${blog._id}`)
+        .expect(204)
+      const response = await api.get('/api/blogs')
+      assert.strictEqual(response.body.length, blogData.blogs.length - 1)
+      const titles = response.body.map(blog => blog.title)
+      assert(!titles.includes(blog.title))
+    })
 
-  await api
-    .post('/api/blogs')
-    .send(newBlogPost)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    test('deleting by malformatted id responds with status code 400', async () => {
+      const errorResponse = await api
+        .delete('/api/blogs/1234abcd')
+        .expect(400)
+      assert(errorResponse.body.error, 'malformatted id')
+      const response = await api.get('/api/blogs')
+      assert.strictEqual(response.body.length, blogData.blogs.length)
+    })
+  })
 
-  const response = await api.get('/api/blogs')
-  const titles = response.body.map(blog => blog.title)
-  assert(titles.includes('New Blog'))
-  assert.strictEqual(response.body.length, blogData.blogs.length + 1)
-})
+  describe('modifying blog posts', () => {
+    test('successful modification of existing blog post', async () => {
+      const blog = blogData.blogs[0]
+      const newBlogPost = {
+        title: 'Updated Title',
+        author: 'Updated Author',
+        url: 'updated.com',
+        likes: 0
+      }
+      await api
+        .put(`/api/blogs/${blog._id}`)
+        .send(newBlogPost)
+        .expect('Content-Type', /application\/json/)
+      const response = await api.get('/api/blogs')
+      assert.strictEqual(response.body.length, blogData.blogs.length)
+      const titles = response.body.map(blog => blog.title)
+      assert(titles.includes('Updated Title'))
+    })
 
-test('creating new blog post without likes defaults number of likes to 0', async () => {
-  const newBlogPost = {
-    title: 'Blog Without Likes',
-    author: 'Test Author',
-    url: 'nolikes.com'
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlogPost)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const response = await api.get('/api/blogs')
-  const addedPost = response.body.find(blog => blog.title === 'Blog Without Likes')
-  assert.strictEqual(addedPost.likes, 0)
-})
-
-test('if title is missing from new blog post, backend responds with status code 400', async () => {
-  const newBlogPost = {
-    author: 'Test Author',
-    url: 'notitle.com',
-    likes: 0
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlogPost)
-    .expect(400)
-})
-
-test('if url is missing from new blog post, backend responds with status code 400', async () => {
-  const newBlogPost = {
-    title: 'Test Title',
-    author: 'Test Author',
-    likes: 0
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlogPost)
-    .expect(400)
+    test('modifying by malformatted id responds with status code 400', async () => {
+      const newBlogPost = {
+        title: 'Updated Title',
+        author: 'Updated Author',
+        url: 'updated.com',
+        likes: 0
+      }
+      const errorResponse = await api
+        .put('/api/blogs/1234abcd')
+        .send(newBlogPost)
+        .expect(400)
+      assert(errorResponse.body.error, 'malformatted id')
+      const response = await api.get('/api/blogs')
+      assert.strictEqual(response.body.length, blogData.blogs.length)
+    })
+  })
 })
 
 after(async () => {
